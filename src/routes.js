93ad7394c4d45d1302f1351b5c82839e5bec2380
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-08 13:09:20 -0300

    Dados

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,23 @@ routes.post('/create_local', async (request, response) => {
     const { rua, bairro, cidade, uf, latitude, longitude } = request.body;
     console.log(rua);
 
-    await connection('locais').insert({
+    const id = await connection('locais').insert({
         rua,
         bairro,
         cidade,
         uf,
         latitude,
         longitude
-    })
+    });
+
+    console.log(id);
+    
     return response.json({ msg: "ok"});
 });
 
 routes.get('/locais', async (request, response) => {
     const query = await connection.queryBuilder().select('*').from('locais');
-    
+
     response.json(query);
 })
 module.exports = routes;
