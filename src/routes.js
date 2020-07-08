6a9ad7edc897f34c35d0f5262754696d8c54464e
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-08 12:57:50 -0300

    Fazendo criação e query dos locais

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,26 @@
 const express = require('express');
-
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    res.json({
-        msg: "ok"
-    });
+const connection = require('./database/connection');
+
+routes.post('/create_local', async (request, response) => {
+    const { rua, bairro, cidade, uf, latitude, longitude } = request.body;
+    console.log(rua);
+
+    await connection('locais').insert({
+        rua,
+        bairro,
+        cidade,
+        uf,
+        latitude,
+        longitude
+    })
+    return response.json({ msg: "ok"});
 });
 
+routes.get('/locais', async (request, response) => {
+    const query = await connection.queryBuilder().select('*').from('locais');
+    
+    response.json(query);
+})
 module.exports = routes;
