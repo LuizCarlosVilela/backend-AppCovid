0c14bc9dd045eaca857fdddd689ef663d0102910
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-14 17:52:24 -0300

    database no pg

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ app.use(express.json());
 app.use(routes);
 
 const PORT = process.env.PORT || 8877;
+const DATABASE_URL = process.env.DATABASE_URL || 8877;
 //Usando para fazer isso  no heroku
 app.listen(PORT, () => {
-    console.log('Escutando na porta: '+ PORT);
+
+    console.log('Escutando na porta: ' + PORT);
+    console.log("Database: "+ ATABASE_URL);
 })
