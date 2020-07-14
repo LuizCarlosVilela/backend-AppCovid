f0ff79beaad28c751fc638441537df1f1f6bf2ea
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-14 17:37:57 -0300

    database no pg

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,15 @@ module.exports = {
 
   development1: {
     client: 'pg',
-    connection: 'postgres://wwgfhaeebomxjj:1b657dae08f0b212d70916ecc4a8aa495f8b98ee5d21f408c3f175ceebc6af77@ec2-107-22-7-9.compute-1.amazonaws.com:5432/db02tove2787qs',
+    connection: {
+      database: 'db02tove2787qs',
+      user:     'wwgfhaeebomxjj',
+      password: '1b657dae08f0b212d70916ecc4a8aa495f8b98ee5d21f408c3f175ceebc6af77'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: "./src/database/migrations"
     },
