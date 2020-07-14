49a1d143df148453a895bdc2585484120d1e9337
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-14 18:31:34 -0300

    database no pg

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      directory: "./src/database/migrations"
+      directory: "app/src/database/migrations"
     },
     useNullAsDefault: "true",
   },
