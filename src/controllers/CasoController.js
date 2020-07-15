5b0c5ece06ad15e4c1258d50c031672973429a39
scsSilva <sallys.carlos93@gmail.com>
2020-07-15 16:57:02 -0300

    Finalizando o backend

--- src/controllers/CasoController.js
+++ src/controllers/CasoController.js
@@ -1,62 +1,61 @@
 const { create, select } = require("./MedicoController");
-const connection = require('../database/connection');
+const connection = require("../database/connection");
 const { connect } = require("../routes");
 
 module.exports = {
-    async create(request, response) {
-        const { id } = request.body;
+  async create(request, response) {
+    const { id } = request.body;
 
-        const { nome_paciente, data_ocorrido, hora_ocorrido } = request.body;
+    const { nome_paciente, data_ocorrido, hora_ocorrido } = request.body;
 
-        const { rua, bairro, cidade, uf, latitude, longitude } = request.body;
+    const { rua, bairro, cidade, uf, latitude, longitude } = request.body;
 
+    const [local_id] = await connection("locais").insert({
+      rua,
+      bairro,
+      cidade,
+      uf,
+      latitude,
+      longitude,
+    });
 
-        const [local_id] = await connection('locais').insert({
-            rua,
-            bairro,
-            cidade,
-            uf,
-            latitude,
-            longitude
-        });
+    console.log("Local " + local_id);
 
-        console.log("Local " + local_id);
+    const [idCaso] = await connection("casos").insert({
+      nome_paciente,
+      data_ocorrido,
+      hora_ocorrido,
+      local_id,
+      medico_id: id,
+    });
 
-        const [idCaso] = await connection('casos').insert({
-            nome_paciente,
-            data_ocorrido,
-            hora_ocorrido,
-            local_id,
-            medico_id: id
-        });
+    console.log("Caso ID " + idCaso);
 
-        console.log("Caso ID " + idCaso);
+    response.json({ msg: "ok" });
+  },
+  async select(request, response) {
+    const casos = await connection("casos").select("*");
 
-        response.json({ msg: "ok" })
-    },
-    async select(request, response) {
+    casos.forEach(async (caso, index) => {
+      const { local_id } = caso;
+      const [local] = await connection("locais")
+        .where("id", local_id)
+        .select("*");
 
-        const casos = await connection('casos').select('*');
+      var newC = {
+        id: caso.id,
+        nome_paciente: caso.nome_paciente,
+        data_ocorrido: caso.data_ocorrido,
+        hora_ocorrido: caso.hora_ocorrido,
+        local,
+        medico_id: caso.medico_id,
+      };
+      console.log("Index" + index);
+      casos[index] = newC;
+    });
 
-        casos.forEach(async (caso, index) => {
-
-            const { local_id } = caso;
-            const [ local ] = await connection('locais').where('id', local_id).select('*');
-
-            var newC = {
-                id: caso.id,
-                nome_paciente: caso.nome_paciente,
-                data_ocorrido: caso.data_ocorrido,
-                hora_ocorrido: caso.hora_ocorrido,
-                local,
-                medico_id: caso.medico_id
-            }
-            console.log("Index" + index);
-            casos[index] = newC;
-        });
-
-        setTimeout( ()=> {
-            response.json(casos);
-        }, 3000)
-    }
-}
+    setTimeout(() => {
+      response.json(casos);
+    }, 2000);
+  },
+};
