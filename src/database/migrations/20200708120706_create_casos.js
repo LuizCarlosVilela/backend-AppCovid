67c0e0890b1ae8ee6d249dd38eecfbf465231e4b
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-16 12:28:01 -0300

    Fazendo migration de casos

---
+++ src/database/migrations/20200708120706_create_casos.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('casos', (table) => {
+        //Id auto incremente
+        table.increments();
+
+        table.string('nome_paciente').notNullable();
+        table.string('data_ocorrido').notNullable();
+        table.string('hora_ocorrido').notNullable();
+
+        table.integer("local_id").notNullable();
+        table.integer("medico_id").notNullable();
+
+        table.foreign('medico_id').references('id').inTable('medicos');
+        table.foreign("local_id").references('id').inTable('locais');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('casos');
+};
