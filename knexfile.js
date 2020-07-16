3e35c60e77025ca40af7aa301eb925242e2ae644
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-07-16 12:22:46 -0300

    Alterações de banco

--- knexfile.js
+++ knexfile.js
@@ -41,5 +41,18 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: "true"
+  }
 
 };
